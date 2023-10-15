d5abbbf21a22442756b335d986f07b982ffa5433
SkillSkullCrasher <144479723+SkillSkullCrasher@users.noreply.github.com>
2023-10-15 19:40:59 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import Search from "./components/Search"
+import MovieCard from "./components/MovieCard"
+
+function App() {
+  
+
+  return (
+    
+    <div>
+      <h1>Movie Search APP</h1>
+      <Search/>
+    </div>
+     
+  )
+}
+
+export default App
